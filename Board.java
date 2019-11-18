01c4e1d3ab260fab23937dc39f544cd37f0f6697
agrosenberger <54327765+agrosenberger@users.noreply.github.com>
2019-11-18 14:13:17 -0600

    Update Board.java

--- Board.java
+++ Board.java
@@ -20,7 +20,7 @@ public class Board extends JPanel {
  
  private final int BOARD_WIDTH = 10;
     private final int BOARD_HEIGHT = 22;
-    private final int PERIOD_INTERVAL = 300;
+    private final int PERIOD_INTERVAL = 500;
 
     private Timer timer;
     private boolean isFallingFinished = false;
